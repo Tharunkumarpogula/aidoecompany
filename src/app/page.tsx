de4c6682c9520a52e0f228f83363e498d6453e5d
Tharunkumarpogula <pogularajtharun@gmail.com>
2026-03-11 18:05:43 +0530

    Remove products section from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import { HeroSection } from '@/components/landing/hero-section';
-import { TechSaaSSection } from '@/components/landing/tech-saas-section';
 
 export default function Home() {
   return (
     <>
       <HeroSection />
-      <TechSaaSSection />
     </>
   );
 }
